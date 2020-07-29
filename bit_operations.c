b082ece2b71704b67203d2774ae7ec2e69684630
Aman <anchandan@gmail.com>
2020-07-28 19:44:32 -0700

    insert bits

--- bit_operations.c
+++ bit_operations.c
@@ -32,14 +32,31 @@ uint8_t reverse_upper_bits(uint8_t num)
 	{
 		reverse_place_holder |= ((place_holder & 1));
 		place_holder >>= 1;
-		reverse_place_holder = reverse_place_holder << 1;
+		reverse_place_holder <<=  1;
 	}
-	reverse_place_holder = reverse_place_holder >> 1;
+	reverse_place_holder >>= 1;
 	reverse_place_holder <<= 4;
 	uint8_t result = (num & reverse_mask) | reverse_place_holder;
 
 	return result;
 
+}
+
+
+/*
+	Insert y in x from bit position a to bit position b. 
+*/
+uint32_t bits_insert(uint32_t x , uint32_t y , uint8_t a , uint8_t b)
+{
+	uint32_t allones = ~0;
+	uint32_t left = allones << (a + 1);
+	uint32_t right = (1 << (b));
+	right -= 1;
+	uint32_t mask = left | right;
+	uint32_t clear_x = x & mask;
+	uint32_t insert_y = y << b;
+
+	return clear_x | insert_y;
 }
 
 int main()
@@ -52,7 +69,9 @@ int main()
 
 	//printf("Turning off bit 2: %d\n", invert_bit(3,1));
 
-	printf("Reverse top 4 bits of 0XBO: %X\n", reverse_upper_bits(0xb0));
+	//printf("Reverse top 4 bits of 0XBO: %X\n", reverse_upper_bits(0xb0));
+
+	printf("Inserting 12 in 32, Expected 44 got %lu\n" , bits_insert(32,12,4,0));
 
 	return 0;
 }
